20d5e6c1db095ef8e9e3738436aa9c32237b74eb
Dario Aguilera <dario_21_06@hotmail.com>
2015-07-23 02:19:03 -0300

    Added (finally) an app icon (Thanks to "Acidrums4"). Update about dialog links.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -113,6 +113,9 @@ MainWindow::MainWindow()
 
     //update window title
     this->updateWindowTitle();
+
+    //Set window icon
+    this->setWindowIcon(QIcon(":qiviewer.png"));
     
     //show or hide menu bar
     this->showHideMenuBar();
